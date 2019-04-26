7d4db0489cc09348210e6b20d79189bdd7343e98
gadimlie <gadimlie@gmail.com>
2019-04-26 19:30:37 +0400

    GST1.0 - First commit. Need a lot work.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gulp Starter Template - GST1.0
+![](https://github.com/gadimlie/gulp-temp/blob/master/public/img/GST1.0.png)
+Author: [Emin Gadimli](http://github.com/gadimlie)
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+'use strict';
+let gulp = require('gulp'),
+    pug  = require('gulp-pug'),
+    sass = require('gulp-sass'),
+    browser = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin');
+
+
+function _pug() {
+    return gulp.src(['./assets/template/*.pug', '!./assets/template/_*.pug'])
+        .pipe(pug({
+                pretty: true
+            }))
+        .on('error',console.log)
+        .pipe(gulp.dest('./public'))
+        .pipe(browser.reload({stream: true}))
+}
+
+function _sass() {
+    return gulp.src('./assets/sass/**/*.+(sass|scss)')
+        .pipe(sass())
+        .on('error', console.log)
+        .pipe(gulp.dest('./public/css'))
+        .pipe(browser.reload({stream: true}))
+    
+}
+function _js() {
+    return gulp.src(['./assets/js/**/*.js', '!./assets/js/vendor/**/*.js'])
+        .pipe(concat('index.js')) 
+        .pipe(gulp.dest('./public/js'))
+        .pipe(browser.reload({stream: true})) 
+}
+
+function _img() {
+    return gulp.src('./assets/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./public/img'))
+        .pipe(browser.reload({stream: true}))
+}
+
+
+function watch() {
+    
+    browser.init({
+        server: {
+            baseDir: './public'
+        }
+    });
+    
+    gulp.watch('assets/sass/**/*.sass', _sass);
+    gulp.watch('assets/template/**/*.pug', _pug);
+    gulp.watch('assets/img/**/*', _img);
+    gulp.watch('assets/js/**/*', _js);
+    
+}
+
+
+
+exports.default = gulp.parallel(
+    gulp.series(_pug, _sass, _js, _img),
+    watch
+);
+
+
